cef362c0c8db771a1639817fc89c23ab47873852
Avnish <avnish@pearlorganisation.com>
2025-01-14 11:16:08 +0530

    ASDSD

--- client/app/page.js
+++ client/app/page.js
@@ -5,7 +5,10 @@ import HomePage from "./_components/HomePage/HomePage";
 export const metadata = {
   title: 'Order Freshly Made Pizza Online in Watford – Customize Your Perfect Pie',
   description: 'Discover the best Watford pizza deals online! Enjoy freshly made pizzas tailored to your taste. Order now and savor your customized pizza experience.',
-}
+  alternates:{
+    canonical: `https://www.pizzainno.com` }
+  }
+
 
 const page = () => {
   
